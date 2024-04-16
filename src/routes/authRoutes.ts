f8a558fbd8b00a60ac59562c6de063de972d845b
Ryan Cavalcanti <contatodevryan@gmail.com>
2024-04-16 19:01:39 -0300

    Changing balance routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { loginUser, verifyToken } from '../controllers/authController';
-import { adicionarSaldo, getUserInfo, obterSaldo } from '../controllers/userController';
+import { getUserInfo } from '../controllers/userController';
 import requireAuth from '../middleware/authMiddleware';
 
 const router = express.Router();
@@ -8,11 +8,5 @@ const router = express.Router();
 router.post('/login', loginUser);
 // Rota protegida para obter informações do usuário autenticado
 router.get('/user', requireAuth, verifyToken, getUserInfo);
-
-// Rota protegida para obter saldo do usuário autenticado
-router.get('/obter-saldo', requireAuth, verifyToken, obterSaldo);
-
-// Rota protegida para adicionar saldo ao usuário autenticado
-router.post('/adicionar-saldo', requireAuth, verifyToken, adicionarSaldo);
 
 export default router;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,18 @@
+// userRoutes.ts
 import express from 'express';
-import { registerUser } from '../controllers/userController';
+import { adicionarSaldo, obterSaldo, registerUser } from '../controllers/userController';
+import requireAuth from '../middleware/authMiddleware';
+import { verifyToken } from '../controllers/authController';
 
 const router = express.Router();
 
 // Rota para cadastrar um novo usuário
 router.post('/register', registerUser);
+
+// Rota protegida para obter saldo do usuário autenticado
+router.get('/obter-saldo', requireAuth, verifyToken, obterSaldo);
+
+// Rota protegida para adicionar saldo ao usuário autenticado
+router.post('/adicionar-saldo', requireAuth, verifyToken, adicionarSaldo);
 
 export default router;
